e45b124e1decc9b05c8c9fb57b795e006876a800
Tahamid Hossain <mitahamid13@gmail.com>
2023-11-03 16:24:07 +0400

    first comitt

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.microsoft.playwright.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+      BrowserType browserType = playwright.chromium();
+      Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+      Page page = browser.newPage();
+      page.navigate("https://www.amazon.ae/");
+      System.out.println(page.title());
+      browser.close();
+    }
+    }
+}
